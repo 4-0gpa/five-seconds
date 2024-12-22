03fd919695dfe441c5f99b721aa97ed6ce539b3a
gongahkia <angryapplegravy@gmail.com>
2024-12-22 14:31:10 +0800

    added pages and api routes

---
+++ src/backend/five_seconds/src/lib/mongodb.js
@@ -0,0 +1,25 @@
+// src/lib/mongodb.js
+
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let clientPromise;
+
+if (process.env.NODE_ENV === 'development') {
+  // In development, use a global variable to prevent creating multiple connections during hot reloading.
+  if (global._mongoClientPromise) {
+    clientPromise = global._mongoClientPromise;
+  } else {
+    global._mongoClientPromise = client.connect();
+    clientPromise = global._mongoClientPromise;
+  }
+} else {
+  // In production, it's safe to connect directly
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
